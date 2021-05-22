9cb7509ce6e5fb626c014201c9e82e4dc6065960
Martin C <caputtimartin12@gmail.com>
2021-05-22 04:23:21 -0300

    Se agrego funcion a boton finalizar y un par de preguntas mas

--- js/tp.js
+++ js/tp.js
@@ -2,6 +2,8 @@
 
 //Variable para saber en que pagina del cuestionario estoy situado 
 var pagina = 0; 
+//Array para recorrer las paginas del cuestionario
+var arrayPaginas = document.getElementsByClassName("pagina");
 
 //creo una funcion que muestra la pagina del formulario que estoy para no repetir tanto el codigo 
 
@@ -10,6 +12,8 @@ function mostrarPagina(numeroDeLaPagina){
   	arrayPaginas[numeroDeLaPagina].style.display = "block";
 }
 
+//Variable para saber el nombre , para poder comenzar y finalizar el cuestionario 
+var nombre ="";
 
 
 //Al seleccionar el cuadro de texto del usuario se borra le nombre para ingresar uno nuevo
@@ -22,7 +26,7 @@ $("#boxUs").focus(function() {
 //para que funcione el boton comenzar:
 $("#butIn").click(function(){
 	
-	var nombre = $("#boxUs").val();
+	nombre = $("#boxUs").val();
 	//tienen que ingresarme un nombre 
 	if(nombre == "" || nombre == "EJ:Edward Nygma"){
 		$("#nomError").html("Forastero identificate!" + "<br/>" + "<br/>" );
@@ -63,16 +67,16 @@ $("#butIn").click(function(){
 $("#butSig").click(function(){
 
 	//Hago que oculte la pagina actual 
-	var arrayPaginas = document.getElementsByClassName("pagina");
 	arrayPaginas[pagina].style.display= "none";
 	
 	//Si la pagina es anterior a la ultima , paso una pagina 
 	if(pagina < arrayPaginas.length-1){
 		pagina = pagina + 1; 
+		document.documentElement.scrollTop = 0;
 	}
 
 	//Muestro la pagina actual (si no avance muestro la pagina que estaba inicialmente)
-	arrayPaginas[pagina].style.display = "block";
+	mostrarPagina(pagina)
 });
 
 //Boton anterior muestra (si hay) la pagina anterior
@@ -85,9 +89,78 @@ $("#butAnt").click(function(){
 	//Si la pagina es la siguiente a la primera , vuelvo una pagina 
 	if(pagina > 0){
 		pagina = pagina - 1; 
+		document.documentElement.scrollTop = 0;
 	}
 
 	//Muestro la pagina actual (si no retrocedi muestro la pagina que estaba inicialmente)
+	mostrarPagina(pagina)
+});
+
+
+
+//Boton Finalizar , muestra las respuestas correctas , las incorrectas y devuelve un resultado  
+$("#butFin").click(function(){
+	//comienza la puntuacion inicial en 0 
+	var puntuacion = 0;
+	//Para saber cuantas preguntas hice en total   
+	var arrayPreguntas = document.getElementsByClassName("pregunta");
+	//Agrego un mensaje segun la puntuacion 
+	var msj ="";
+
+
+
+	arrayPaginas[pagina].style.display= "none";
+	pagina = 3;
 	arrayPaginas[pagina].style.display = "block";
+
+	// checkeo en cada pregunta si contesto bien se suma un punto.
+	if(document.getElementById("preg1JG").checked){
+		puntuacion++;
+	}
+
+	
+	if(document.getElementById("SuperCiudad").selectedIndex == "4"){
+		puntuacion++;
+	}
+
+	if(document.getElementById("cbJL1").checked && document.getElementById("cbJL4").checked){
+		puntuacion++;
+	}
+
+
+	if(document.getElementById("pregDM3").checked){
+		puntuacion++;
+	}
+
+
+	if(document.getElementById("superLogo").selectedIndex == "8"){
+		puntuacion++;
+	}
+
+	if(document.getElementById("cbJT1").checked && document.getElementById("cbJT3").checked && document.getElementById("cbJT5").checked){
+		puntuacion++;
+	}
+
+	if(document.getElementById("pregMM3").checked){
+		puntuacion++;
+	}
+
+
+	if(document.getElementById("victorStone").selectedIndex == "4"){
+		puntuacion++;
+	}
+
+
+
+
+	//Luego de saber la puntuacion total defino el msj 
+	if( (arrayPreguntas.length/puntuacion) >2 ){
+		msj="ouch " + nombre + " :/ ";
+	}else{
+		msj="Felicidades " + nombre +" eres una persona de cultura ;)";
+	}
+
+
+	$("#puntuacion").html("Contesto bien " + puntuacion+" de " + arrayPreguntas.length +" preguntas <br> " + msj);
 
 });
